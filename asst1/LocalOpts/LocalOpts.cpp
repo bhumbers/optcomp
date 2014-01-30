c639f59160f6d8f917626ef8affcf3062567ad87
user <user@15-745-s14.(none)>
2014-01-30 01:01:18 -0500

    Fixed bug where we try to bit shift opt after mult op is already applied

--- asst1/LocalOpts/LocalOpts.cpp
+++ asst1/LocalOpts/LocalOpts.cpp
@@ -43,83 +43,71 @@ class FunctionInfo : public ModulePass {
           for (BasicBlock::iterator bbIter = funIter->begin(); bbIter != funIter->end(); ++bbIter) {
             //If looking at a binary operator, apply one of the opts for this question
             if (BinaryOperator* binInst = dyn_cast<BinaryOperator>(&*bbIter)) {   
-                //Extract constant integer operands if able
-                ConstantInt* constIntA = dyn_cast<ConstantInt>(binInst->getOperand(0));
-                ConstantInt* constIntB = dyn_cast<ConstantInt>(binInst->getOperand(1));
+              //Extract constant integer operands if able
+              ConstantInt* constIntA = dyn_cast<ConstantInt>(binInst->getOperand(0));
+              ConstantInt* constIntB = dyn_cast<ConstantInt>(binInst->getOperand(1));
 
-                //If both operands are const ints, we can do constant folding and replace with the expression w/ eval result
-                if (constIntA && constIntB) {
-                  const APInt& constIntValA = constIntA->getValue();
-                  const APInt& constIntValB = constIntB->getValue();
+              //If both operands are const ints, we can do constant folding and replace with the expression w/ eval result
+              if (constIntA && constIntB) {
+                const APInt& constIntValA = constIntA->getValue();
+                const APInt& constIntValB = constIntB->getValue();
 
-                  ConstantInt* evalConst = 0;
-                  switch (binInst->getOpcode()) {
-                    case Instruction::Add: evalConst = ConstantInt::get(constIntA->getContext(), constIntValA + constIntValB); break;
-                    case Instruction::Sub: evalConst = ConstantInt::get(constIntA->getContext(), constIntValA - constIntValB); break;
-                    case Instruction::Mul: evalConst = ConstantInt::get(constIntA->getContext(), constIntValA * constIntValB); break;
-                  }
-                  if (evalConst) {
-                    std::cout << "Const-folded an expression: " << binInst->getName().str() << " " << constIntValA.toString(10, true) << ", " + constIntValB.toString(10, true) << std::endl;
-                    ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, evalConst);
-                    numAlgIdentityOpts++;
-                  }
+                ConstantInt* evalConst = 0;
+                switch (binInst->getOpcode()) {
+                  case Instruction::Add: evalConst = ConstantInt::get(constIntA->getContext(), constIntValA + constIntValB); break;
+                  case Instruction::Sub: evalConst = ConstantInt::get(constIntA->getContext(), constIntValA - constIntValB); break;
+                  case Instruction::Mul: evalConst = ConstantInt::get(constIntA->getContext(), constIntValA * constIntValB); break;
                 }
-                //Otherwise, if at least one is a constant integer, see what else we can optimize...
-                else if (constIntA || constIntB) {
-                  ConstantInt* constIntTerm = (constIntA) ? constIntA : constIntB; //grab the const term for this binary op
-                  Value* otherTerm = (constIntA) ? binInst->getOperand(1) : binInst->getOperand(0); //grab the other "value" term
-                  
-                  //Apply any algebraic identity opts
-                  switch (binInst->getOpcode()) {
-                    case Instruction::Add:
-                      // Additive identity: x + 0 = x
-                      if (constIntTerm->isZero()) {
-                        ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, otherTerm);
-                        std::cout << "Optimized an instance of additive identity." << std::endl;
-                        numAlgIdentityOpts++;
-                      }
-                      break;
-                    case Instruction::Mul:
-                      // Multiplicative identity: x * 1 = 1
-                      if (constIntTerm->isOne()) {
-                        ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, otherTerm);
-                        std::cout << "Optimized an instance of multiplicative identity." << std::endl;
-                        numAlgIdentityOpts++;
-                      }
-                      break;
-                    case Instruction::UDiv:
-                    case Instruction::SDiv:
-                      //Division identity: x / 1 = x
-                      //(Note: not commutative... left term must be variable & right term to be 1)
-                      if (constIntB && constIntB->isOne()) {
-                        ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, binInst->getOperand(0));
-                        std::cout << "Optimized an instance of division identity." << std::endl;
-                        numAlgIdentityOpts++;
-                      }
-                      break;
+                if (evalConst) {
+                  std::cout << "Const-folded an expression: " << binInst->getName().str() << " " << constIntValA.toString(10, true) << ", " + constIntValB.toString(10, true) << std::endl;
+                  ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, evalConst);
+                  numAlgIdentityOpts++;
+                }
+              }
+              //Otherwise, if at least one is a constant integer, see what else we can optimize...
+              else if (constIntA || constIntB) {
+                ConstantInt* constIntTerm = (constIntA) ? constIntA : constIntB; //grab the const term for this binary op
+                Value* otherTerm = (constIntA) ? binInst->getOperand(1) : binInst->getOperand(0); //grab the other "value" term
+                
+                //Apply any algebraic identity opts
+                switch (binInst->getOpcode()) {
+                  case Instruction::Add:
+                    // Additive identity: x + 0 = x
+                    if (constIntTerm->isZero()) {
+                      ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, otherTerm);
+                      std::cout << "Optimized an instance of additive identity." << std::endl;
+                      numAlgIdentityOpts++;
                     }
-                  }
+                    break;
+                  case Instruction::Mul:
+                    // Multiplicative identity: x * 1 = 1
+                    if (constIntTerm->isOne()) {
+                      ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, otherTerm);
+                      std::cout << "Optimized an instance of multiplicative identity." << std::endl;
+                      numAlgIdentityOpts++;
+                    }
+                    else if (constIntTerm->getValue().isPowerOf2()) {
+                      const int64_t constIntVal = constIntTerm->getSExtValue();
+                      Value* shiftVal = ConstantInt::get(constIntTerm->getType(), log2(constIntVal));
 
-                //Handle strength reductions where possible
-                if (constIntA || constIntB)
-                {
-                  ConstantInt* constIntTerm = (constIntA) ? constIntA : constIntB; //grab the const term for this binary op
-                  Value* otherTerm = (constIntA) ? binInst->getOperand(1) : binInst->getOperand(0); //grab the other "value" term
-                  //Strength Reduction for identifying multiplication by 2 and converting to left shift
-                  if (binInst->getOpcode() == Instruction::Mul && constIntTerm->getValue().isPowerOf2())
-                  { 
-                    //TODO: Handle correct power of two shift
-                    const int64_t constIntVal = constIntTerm->getSExtValue();
-                    Value* shiftVal = ConstantInt::get(constIntTerm->getType(), log2(constIntVal));
+                      std::cout << "About to apply a bitshift: " << constIntVal << ", " << log2(constIntVal) << std::endl;
 
-                    std::cout << "About to apply a bitshift: " << constIntVal << ", " << log2(constIntVal) << std::endl;
-
-                    ReplaceInstWithInst(binInst->getParent()->getInstList(),bbIter, BinaryOperator::Create(Instruction::Shl, otherTerm, shiftVal, "shl", (Instruction*)(0)));
-                    numStrengthRedOpts++;
-                  }
+                      ReplaceInstWithInst(binInst->getParent()->getInstList(),bbIter, BinaryOperator::Create(Instruction::Shl, otherTerm, shiftVal, "shl", (Instruction*)(0)));
+                      numStrengthRedOpts++;
+                    }
+                    break;
+                  case Instruction::UDiv:
+                  case Instruction::SDiv:
+                    //Division identity: x / 1 = x
+                    //(Note: not commutative... left term must be variable & right term to be 1)
+                    if (constIntB && constIntB->isOne()) {
+                      ReplaceInstWithValue(binInst->getParent()->getInstList(), bbIter, binInst->getOperand(0));
+                      std::cout << "Optimized an instance of division identity." << std::endl;
+                      numAlgIdentityOpts++;
+                    }
+                    break;
                 }
-                //TODO: Handle strength reductions
-                //TODO: Handle floating point identities & const folding?
+              }
             }
           }
         }
