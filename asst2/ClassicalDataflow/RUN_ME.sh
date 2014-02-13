24e531b76547966bafd9bce9a5d75fd7809da624
Ben Humberston <bhumbers@cs.cmu.edu>
2014-02-13 18:55:18 -0500

    Added pass make & status updates to run script

--- asst2/ClassicalDataflow/RUN_ME.sh
+++ asst2/ClassicalDataflow/RUN_ME.sh
@@ -1,5 +1,13 @@
 #/bin/bash
 
+#Build the passes
+echo "Building liveness & reaching definition passes..."
+make
+echo "DONE building passes"
+
+#Run the tests
+echo "Building & running tests"
 ./build_and_dis_tests.sh
 ./do_liveness_tests.sh
 ./do_reaching_tests.sh
+echo "DONE building & running tests"
