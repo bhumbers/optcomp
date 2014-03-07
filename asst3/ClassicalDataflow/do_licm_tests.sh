1ba9872b6131695cf1ba977192d7e3098806700e
Ben Humberston <bhumbers@cs.cmu.edu>
2014-03-06 19:14:10 -0500

    A3: Updated LICM script to run profiles

--- asst3/ClassicalDataflow/do_licm_tests.sh
+++ asst3/ClassicalDataflow/do_licm_tests.sh
@@ -1,7 +1,17 @@
 #/bin/bash
 
-for f in *.bc
+for f in *_main.bc
 do
-  echo "Running LICM test: $f"
-  opt -load ./loop-invariant-code-motion.so -loop-simplify -cd-licm $f -o /dev/null 
+  echo "Running LICM benchmark: $f"
+
+  fileNoPath=${f##*/}
+  optimizedFile=${fileNoPath%.*}-opt.bc
+
+  echo "Pre-optimization results: "
+  lli -stats -force-interpreter $f
+
+  opt -load ./loop-invariant-code-motion.so -loop-simplify -cd-licm $f -o $optimizedFile
+
+  echo "Post-optimization results: "
+  lli -stats -force-interpreter $optimizedFile
 done
